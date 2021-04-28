a67ac8ff72af8d66e6eebb7338a10295b3443a36
mafugo <83337271+mafugo@users.noreply.github.com>
2021-04-28 12:04:18 +0300

    Update ForgotPasswordActivity.java

--- ForgotPasswordActivity.java
+++ ForgotPasswordActivity.java
@@ -1,7 +1,8 @@
 package com.omerkagandanaci.projecttrial.View;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,16 +11,18 @@ import com.omerkagandanaci.projecttrial.R;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
     FirebaseAuth auth;
+    Button buttonReset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-    }
-
-
-    public void forgetPassword(){
-        auth = FirebaseAuth.getInstance();
-        auth.sendPasswordResetEmail(((EditText) findViewById(R.id.editTextTextEmailAddress)).getText().toString());//.addOnCompleteListener(
+        buttonReset = findViewById(R.id.buttonReset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                auth = FirebaseAuth.getInstance();
+//        auth.sendPasswordResetEmail(((EditText) findViewById(R.id.editTextTextEmailAddress)).getText().toString());//.addOnCompleteListener(
 //        ForgotPasswordActivity.this,new OnCompleteListener<AuthResult>(){
 //
 //            @Override
@@ -31,6 +34,10 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 //                    Toast.makeText(ForgotPasswordActivity.this, "Something went wrong \n Try again", Toast.LENGTH_LONG).show();
 //            }
 //        });
+            }
+        });
     }
+}    
+    
 
-}
+    
